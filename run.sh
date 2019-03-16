0b005411fc7f617ec0a67453903e496b8fc90022
natel9178 <natel9178@gmail.com>
2019-03-16 14:30:05 -0700

    Changed vocab generation

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ elif [ "$1" = "test" ]; then
     touch outputs/test_outputs.txt
     CUDA_VISIBLE_DEVICES=0 python run.py decode model.bin ./data/test.src ./data/test.tgt outputs/test_outputs.txt --cuda
 elif [ "$1" = "vocab" ]; then
-	python vocab.py --train-src=./data/train.src --train-tgt=./data/train.tgt vocab.json
+	python vocab.py --train-src=./data/train.src --train-tgt=./data/train.tgt --freq-cutoff=10 vocab.json
 elif [ "$1" = "dump" ]; then
     mkdir -p data
     python3 dump_pickle.py -train pickles/train.pkl -valid pickles/val.pkl -train_save data/train -valid_save data/valid -test pickles/test.pkl -test_save data/test
