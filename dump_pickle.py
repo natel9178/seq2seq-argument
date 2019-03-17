bbbcaa232ee38f8a269c632f50d281262b07679d
natel9178 <natel9178@gmail.com>
2019-03-16 22:33:01 -0700

    added trim length

--- dump_pickle.py
+++ dump_pickle.py
@@ -1,13 +1,13 @@
 import _pickle as pickle
 import argparse
 
-def process(pickle, save_file, keep_case):
+def process(pickle, save_file, keep_case, trim_length=50):
   with open(save_file+'.src', "a") as src_file:
     with open(save_file+'.tgt', "a") as tgt_file:
       for discussion in pickle:
         for src, tgt in zip(discussion['src'], discussion['tgt']):
-          src_str = " ".join(src)
-          tgt_str = " ".join(tgt)
+          src_str = " ".join(src[:trim_length])
+          tgt_str = " ".join(tgt[:trim_length])
 
           if not keep_case:
             src_str = src_str.lower()
@@ -25,17 +25,18 @@ def main():
   parser.add_argument('-valid_save', required=True)
   parser.add_argument('-test', required=True)
   parser.add_argument('-test_save', required=True)
+  parser.add_argument('-trim_length', type=int, default=50)
   parser.add_argument('-keep_case', action='store_true')
   opt = parser.parse_args()
 
   train = pickle.load(open( opt.train, "rb" ))
-  process(train, opt.train_save, opt.keep_case)
+  process(train, opt.train_save, opt.keep_case, opt.trim_length)
 
   valid = pickle.load(open( opt.valid, "rb" ))
-  process(valid, opt.valid_save, opt.keep_case)
+  process(valid, opt.valid_save, opt.keep_case, opt.trim_length)
   
   test = pickle.load(open( opt.test, "rb" ))
-  process(test, opt.test_save, opt.keep_case)
+  process(test, opt.test_save, opt.keep_case, opt.trim_length)
 
 
 
