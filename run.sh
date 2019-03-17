2b645192246bfb6acf2e52a1d76e889add823ad9
natel9178 <natel9178@gmail.com>
2019-03-17 16:23:48 -0700

    more data

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 if [ "$1" = "train" ]; then
 	CUDA_VISIBLE_DEVICES=0 python run.py train --train-src=./data/train.src --train-tgt=./data/train.tgt \
-        --dev-src=./data/valid.src --dev-tgt=./data/valid.tgt --vocab=vocab.json --cuda --batch-size=1 --no-char-decoder
+        --dev-src=./data/valid.src --dev-tgt=./data/valid.tgt --vocab=vocab.json --cuda --batch-size=2 --no-char-decoder
 elif [ "$1" = "test" ]; then
     mkdir -p outputs
     touch outputs/test_outputs.txt
