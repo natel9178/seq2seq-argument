2420596e45a9b00770bac2385e8ee101933471b2
natel9178 <natel9178@gmail.com>
2019-03-17 19:54:17 -0700

    more power

--- run.sh
+++ run.sh
@@ -2,16 +2,16 @@
 
 if [ "$1" = "train" ]; then
 	CUDA_VISIBLE_DEVICES=0 python run.py train --train-src=./data/train.src --train-tgt=./data/train.tgt \
-        --dev-src=./data/valid.src --dev-tgt=./data/valid.tgt --vocab=vocab.json --cuda --batch-size=36
+        --dev-src=./data/valid.src --dev-tgt=./data/valid.tgt --vocab=vocab.json --cuda --batch-size=64
 elif [ "$1" = "test" ]; then
     mkdir -p outputs
     touch outputs/test_outputs.txt
     CUDA_VISIBLE_DEVICES=0 python run.py decode model.bin ./data/test.src ./data/test.tgt outputs/test_outputs.txt --cuda
 elif [ "$1" = "vocab" ]; then
-	python vocab.py --train-src=./data/train.src --train-tgt=./data/train.tgt --freq-cutoff=10 --size=16000 vocab.json
+	python vocab.py --train-src=./data/train.src --train-tgt=./data/train.tgt --freq-cutoff=5 --size=16000 vocab.json
 elif [ "$1" = "dump" ]; then
     mkdir -p data
-    python3 dump_pickle.py -train pickles/train.pkl -valid pickles/val.pkl -train_save data/train -valid_save data/valid -test pickles/test.pkl -test_save data/test
+    python3 dump_pickle.py -train pickles/train.pkl -valid pickles/val.pkl -train_save data/train -valid_save data/valid -test pickles/test.pkl -test_save data/test -trim_length 50
 else
 	echo "Invalid Option Selected"
 fi
